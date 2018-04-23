9603a227cd9acc2da545055040dc85adb814daaa
Eliah7 <Guest@Eliahs-mac-2.local>
2018-04-23 12:16:47 +0300

    POS

---
+++ pos_tagging.py
@@ -0,0 +1,16 @@
+from keras.layers.core import Activation, Dense, Dropout, RepeatVector, SpatialDropout1D
+from keras.layers.embeddings import Embedding
+from keras.layers.recurrent import GRU
+from keras.layers.wrappers import TimeDistributed
+from keras.models import Sequential
+from keras.preprocessing import sequence
+from keras.utils import np_utils
+from sklearn.model_selection import train_test_split
+import collections
+import nltk
+import numpy as np
+import os
+
+fedata = open('treebank_sents.txt', 'wb')
+ffdata = open('treebank_poss.txt', 'wb')
+
